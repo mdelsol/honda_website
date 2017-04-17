b1baeba5da57c8d7b6294a8fdd586ae0f5e06738
Matt Smid <msmid@email.uscupstate.edu>
2017-04-17 13:35:23 +0000

    Advanced login

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'sessions/new'
 
   get 'users/new'
 
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-  
-  get  'static_pages/about'
-
-  root 'static_pages#home'
-  
-  post '/signup',  to: 'users#create'
-  
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  post '/signup',  to: 'users#create'
   
   resources :users
 end
